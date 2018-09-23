5a3e9ead5a5830d6510ec0dd3712ed1f9326d402
Mithoon Kumar <mithoonk@zeta.tech>
2018-09-23 20:10:57 +0530

    First commit

---
+++ code/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<list>
+#include<set>
+#include<unordered_map>
+#include<sstream>
+#include<limits.h>
+#include<stdio.h>
+using namespace std;
+#define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+
+int main(){
+    freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
+    faster;
+    
+    
+    return 0;
+}
