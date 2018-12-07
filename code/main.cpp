e09fc6bce06e6bf6bde7463c4dff3be8c311ff96
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-07 18:02:39 +0530

    Complete implementation

--- code/main.cpp
+++ code/main.cpp
@@ -15,9 +15,27 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
+int dpCalculate(int n, int k) {
+    int arr[n+1][k+1];
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= min(j,k); j++) {
+            if (j == 0 ) {
+                arr[i][j] = 1;
+            } else {
+                arr[i][j] = arr[i-1][j] + (j* arr[i-1][j-1]);
+            }
+            arr[i][j+1] = 0;
+        }
+    }
+    return arr[n][k];
+}
+
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    
+    int n,k;
+    n = 10;
+    k = 2;
+    cout<<dpCalculate(n, k);
     return 0;
 }
