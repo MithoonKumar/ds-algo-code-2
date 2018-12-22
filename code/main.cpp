693fe5319b627eaf76d204bd20d6ea080fe8ac85
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-22 17:33:02 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -14,79 +14,43 @@
 #include<stdio.h>
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
-void heapifyMin(int index);
-int heapArr[1000];
-int heapSize = 0;
 
-int parent(int i){
-    return (i-1)/2;
-}
-
-int leftChild(int i) {
-    return 2*i+1;
-}
-
-int rightChild(int i) {
-    return 2*i+2;
-}
-
-void swap(int pos1, int pos2) {
-    int temp = heapArr[pos1];
-    heapArr[pos1] = heapArr[pos2];
-    heapArr[pos2] = temp;
-}
-
-void insertInMinHeap(int num){
-    heapArr[heapSize] = num;
-    heapSize++;
-    int pos = heapSize-1;
-    while (pos!=0 && heapArr[parent(pos)] > heapArr[pos]) {
-        swap(parent(pos), pos);
+void breadthFirstSearch(int node, int n, vector<int> adj[]) {
+    int visited[n];
+    for(int i=0; i<n; i++) {
+        visited[i] = 0;
     }
-}
-
-void deleteMin() {
-    if (heapSize == 0) {
-        return;
+    queue<int>q;
+    q.push(node);
+    visited[node] = 1;
+    while(q.size()>0) {
+        int topNode = q.front();
+        cout<<topNode<<" ";
+        q.pop();
+        for(int i=0; i<adj[topNode].size(); i++) {
+            if (visited[adj[topNode][i]] == 0) {
+                q.push(adj[topNode][i]);
+                visited[adj[topNode][i]] = 1;
+            }
+        }
     }
-    int root = heapArr[heapSize-1];
-    heapArr[0] = root;
-    heapSize--;
-    heapifyMin(0);
-}
-
-void heapifyMin(int index) {
-    int val = heapArr[index];
-    int leftChildVal = leftChild(index)<heapSize-1 ? heapArr[leftChild(index)] : INT_MAX;
-    int rightChildVal = rightChild(index)<heapSize-1 ? heapArr[rightChild(index)] :INT_MAX;
-    int min = (val < leftChildVal ? val : leftChildVal) < rightChildVal ? (val < leftChildVal ? val : leftChildVal) : rightChildVal;
-    if (val == min) {
-        return;
-    } else if (min == leftChildVal) {
-        swap(index, leftChild(index));
-        heapifyMin(leftChild(index));
-    } else {
-        swap(index, rightChild(index));
-        heapifyMin(rightChild(index));
-    }
+    
 }
 
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    int n;
-    cin>>n;
-    for (int i=0; i<n; i++) {
-        int temp;
-        cin>>temp;
-        insertInMinHeap(temp);
-    }
-    deleteMin();
-    
-    for(int i=0; i<heapSize; i++) {
-        cout<<heapArr[i] <<" ";
+    int n, e;
+    cin>>n>>e;
+    vector<int> adj[n];
+    for (int i=0; i<e; i++) {
+        int a, b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        //adj[b].push_back(a);
     }
     
+    breadthFirstSearch(0, n, adj);
     return 0;
 }
 
