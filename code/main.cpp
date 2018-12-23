3143b8a1a515104166e87730448f3c6f6d04fc88
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-23 17:55:45 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -15,56 +15,65 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
-int findMinDistIndex(int visited[], int distances[], int len ) {
-    int min = INT_MAX;
-    int index = -1;
-    for (int i=0; i<len; i++) {
-        if (!visited[i] && distances[i]<min) {
-            min = distances[i];
-            index = i;
+struct trieNode {
+    bool isEndOfWord;
+    trieNode * chidlren[26];
+    trieNode() {
+        this->isEndOfWord = false;
+        for (int i=0; i<26; i++) {
+            this->chidlren[i] = NULL;
         }
     }
-    return index;
+};
+
+
+
+
+void insert(string str, int pos, trieNode * root) {
+    if (root->chidlren[str[pos]-'a'] == NULL) {
+        root->chidlren[str[pos]-'a'] = new trieNode();
+    }
+    if (pos+1 == str.length()) {
+        root->chidlren[str[pos]-'a']->isEndOfWord = true;
+    } else {
+        insert(str, pos+1, root->chidlren[str[pos]-'a']);
+    }
 }
 
-
-void dijkastra(int matrix[9][9], int source) {
-    int distances[9];
-    int visited[9] = {};
-    for (int i=0; i<9; i++) {
-        distances[i] = INT_MAX;
-    }
-    distances[source] = 0;
-    int loop = 9-1;
-    while(loop) {
-        int index = findMinDistIndex(visited, distances, 9);
-        visited[index] = 1;
-        for (int i=0; i<9; i++) {
-            if (matrix[index][i] && distances[i]>distances[index] + matrix[index][i]) {
-                distances[i] = distances[index] + matrix[index][i];
+bool searchT(string str, int pos, trieNode * root) {
+    if (!root->chidlren[str[pos]-'a']) {
+        return false;
+    } else {
+        if (pos+1 == str.length()) {
+            if (root->chidlren[str[pos]-'a']->isEndOfWord) {
+                return true;
+            } else {
+                return false;
             }
+        } else {
+            return searchT(str, pos+1, root->chidlren[str[pos]-'a']);
         }
-        loop--;
-    }
-    for(int i=0; i<9; i++) {
-        cout<<"Minimum distance of vertex " << i <<" from " <<source<< " is " << distances[i]<<endl;
     }
 }
+
 
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    int adjMatrix[9][9] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-        {4, 0, 8, 0, 0, 0, 0, 11, 0},
-        {0, 8, 0, 7, 0, 4, 0, 0, 2},
-        {0, 0, 7, 0, 9, 14, 0, 0, 0},
-        {0, 0, 0, 9, 0, 10, 0, 0, 0},
-        {0, 0, 4, 14, 10, 0, 2, 0, 0},
-        {0, 0, 0, 0, 0, 2, 0, 1, 6},
-        {8, 11, 0, 0, 0, 0, 1, 0, 7},
-        {0, 0, 2, 0, 0, 0, 6, 7, 0}
-    };
-    dijkastra(adjMatrix, 0);
+    int n;
+    cin>>n;
+    string strings[n];
+    trieNode * root = new trieNode();
+    for (int i=0; i<n; i++) {
+        cin>>strings[i];
+        insert(strings[i],0, root);t
+    }
+    
+    searchT("the",0, root)? cout<<"found"<<endl :cout<< "not found"<<endl;
+    searchT("these",0, root)? cout<<"found"<<endl :cout<< "not found"<<endl;
+    searchT("their", 0, root)?cout<< "found"<<endl :cout<< "not found"<<endl;
+    searchT("thaw", 0, root)?cout<< "found"<<endl :cout<< "not found"<<endl;
+    
     return 0;
 }
 
