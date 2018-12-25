fcbeee5615719a8623a4ccedf3979e71288d80e3
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-25 22:46:16 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -14,100 +14,30 @@
 #include<stdio.h>
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
-int matrix[100][100];
 
-void rotateMatrix(int r, int c, int size) {
-    int row = r, col = c;
-    vector<int>nums;
-    for(int i=r; i<=r; i++) {
-        for(int j=c; j<=c+size-2; j++) {
-            nums.push_back(matrix[i][j]);
-        }
+bool binarySearch(int arr[], int num, int s, int e) {
+    if (s > e) {
+        return false;
     }
-    c = c+size-1;
-    for(int i=r; i<=r+size-2; i++) {
-        for(int j=c; j<=c; j++) {
-            nums.push_back(matrix[i][j]);
-        }
-    }
-    r = r+size-1;
-    for(int i=r; i<=r; i++) {
-        for(int j=c; j>=c-size+2; j--) {
-            nums.push_back(matrix[i][j]);
-        }
-    }
-    c = c-size+1;
-    for(int i=r; i>=r-size+2; i--) {
-        for(int j=c; j<=c; j++) {
-            nums.push_back(matrix[i][j]);
-        }
-    }
-    vector<int> rotatedVec;
-    rotatedVec.push_back(nums[nums.size()-1]);
-    for (int i=0; i<nums.size()-1; i++) {
-        rotatedVec.push_back(nums[i]);
-    }
-    rotatedVec.push_back(nums[0]);
-    int count = 0;
-    for(int i=row; i<=row; i++) {
-        for(int j=col; j<=col+size-2; j++) {
-            matrix[i][j] = rotatedVec[count++];
-        }
-    }
-    col = col + size -1;
-    for(int i=row; i<=row+size-2; i++) {
-        for(int j=col; j<=col; j++) {
-            matrix[i][j] = rotatedVec[count++];
-        }
-    }
-    row = row + size -1;
-    for(int i=row; i<=row; i++) {
-        for(int j=col; j>=col-size+2; j--) {
-            matrix[i][j] = rotatedVec[count++];
-        }
-    }
-    col = col - size + 1;
-    for(int i=row; i>=row-size+2; i--) {
-        for(int j=col; j<=col; j++) {
-            matrix[i][j] = rotatedVec[count++];
-        }
-    }
-    
-    row = row - size + 1;
-    size = size - 2;
-    if (size >= 2) {
-        rotateMatrix(row+1, col+1, size);
+    int mid = (s+e)/2;
+    if (arr[mid] == num ){
+        return true;
+    } else if (num>arr[mid]) {
+        return binarySearch(arr, num, mid+1, e);
+    } else {
+        return binarySearch(arr, num, s, mid-1);
     }
 }
 
 int main(){
-    freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
+    //freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    int n;
-    cin>>n;
-    for (int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cin>>matrix[i][j];
-        }
+    int arr[] ={1, 2, 3, 4, 5, 6, 18, 19, 33, 34, 89, 101};
+    while(true) {
+        int temp;
+        cin>>temp;
+        cout<<binarySearch(arr, temp, 0, 11);
     }
-    
-    for (int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl;
-    
-    rotateMatrix(1, 1, n);
-    
-    for (int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    
     
     return 0;
 }
