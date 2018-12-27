59fadea61cc42fc6f776d7d1d195b88d20bc926c
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-27 22:27:29 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -15,54 +15,84 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
-int matrix[100][100];
+bool matrix[100][100];
 
-int kadane(vector<int>vec) {
-    int sum = 0;
-    int maxSum = INT_MIN;
-    for(int i=0; i<vec.size(); i++) {
-        if (sum+vec[i]>=0) {
-            sum = sum + vec[i];
-        } else {
-            sum = 0;
-        }
-        maxSum = sum>maxSum?sum:maxSum;
+struct point {
+    int r,c,distance;
+    point(int a, int b, int d) {
+        this->r = a;
+        this->c = b;
+        this->distance = d;
     }
-    return maxSum;
-}
+};
+
 
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    int r, c;
-    cin>>r>>c;
-    for(int i=1; i<=r; i++) {
-        for (int j=1; j<=c; j++) {
+    int m, n;
+    cin>>m>>n;
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
             cin>>matrix[i][j];
         }
     }
     
-    int maxVal = INT_MIN;
-    for (int m=1; m<=c; m++) {
-        for (int n=1; n<=c; n++) {
-            vector<int>tempVec;
-            for(int i=1; i<=n; i++) {
-                int sum = 0;
-                for (int j=m; j<=n; j++){
-                    sum+=matrix[i][j];
-                }
-                tempVec.push_back(sum);
-            }
-            int returnedVal = kadane(tempVec);
-            maxVal = maxVal<returnedVal?returnedVal:maxVal;
+    int sr, sc, dr, dc;
+    cin>>sr>>sc>>dr>>dc;
+    if (sr == dr && sc == dc) {
+        cout<<0;
+        return 0;
+    }
+    queue<point> q;
+    point starting =  point(sr, sc, 0);
+    q.push(starting);
+    int finalDistance = -1;
+    bool visited[m][n];
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            visited[i][j] = false;
         }
     }
-    
-    cout<<maxVal<<endl;
-    
-    
+    while(q.size()) {
+        point p = q.front();
+        q.pop();
+        point p1 = point(p.r-1, p.c, p.distance+1);
+        point p2 = point(p.r+1, p.c, p.distance+1);
+        point p3 = point(p.r, p.c-1, p.distance+1);
+        point p4 = point(p.r, p.c+1, p.distance+1);
+        vector<point>points;
+        points.push_back(p1);
+        points.push_back(p2);
+        points.push_back(p3);
+        points.push_back(p4);
+        for (int z=0; z<points.size(); z++) {
+            point tempPoint = points[z];
+            int i = tempPoint.r;
+            int j = tempPoint.c;
+            if ((i>=0 && j>=0 && i<n && j<n) && !visited[i][j] && matrix[i][j]) {
+                visited[i][j] = true;
+                point tempPoint = point(i, j, p.distance+1);
+                if (tempPoint.r == dr && tempPoint.c == dc) {
+                    finalDistance = p.distance+1;
+                } else {
+                    q.push(tempPoint);
+                }
+            }
+        }
+        
+ 
+        if (finalDistance != -1) {
+            break;
+        }
+    }
+    if (finalDistance == -1) {
+        cout<<"Not reachable"<<endl;
+    } else {
+        cout<<finalDistance<<endl;
+    }
     return 0;
 }
 
 
-    
+
