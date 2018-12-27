2ff15bc9778030b25d682644a1381b809c3090e0
Mithoon Kumar <mithoonk@zeta.tech>
2018-12-27 19:40:46 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -15,59 +15,54 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
+int matrix[100][100];
+
+int kadane(vector<int>vec) {
+    int sum = 0;
+    int maxSum = INT_MIN;
+    for(int i=0; i<vec.size(); i++) {
+        if (sum+vec[i]>=0) {
+            sum = sum + vec[i];
+        } else {
+            sum = 0;
+        }
+        maxSum = sum>maxSum?sum:maxSum;
+    }
+    return maxSum;
+}
+
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    int n,k;
-    cin>>n>>k;
-    int matrix[n+1][n+1];
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=n; j++) {
+    int r, c;
+    cin>>r>>c;
+    for(int i=1; i<=r; i++) {
+        for (int j=1; j<=c; j++) {
             cin>>matrix[i][j];
         }
     }
-    int verticalSumMatrix[n+1][n+1];
-    for(int j=1; j<=n; j++) {
-        int tempSum = 0;
-        for (int i=1;i<=k; i++) {
-            tempSum+=matrix[i][j];
-        }
-        verticalSumMatrix[1][j] = tempSum;
-    }
     
-    for(int j=1; j<=n; j++) {
-        for(int i=2; i<=n-k+1; i++) {
-            verticalSumMatrix[i][j] = verticalSumMatrix[i-1][j] + matrix[i][j] - matrix[i-k+1][j];
-        }
-    }
-    
-    int answer = INT_MIN;
-    int x = -1, y = -1;
-    for(int i=1; i<=n-k+1; i++) {
-        int tempSum = 0;
-        for(int l = 1; l<=k; l++) {
-            tempSum+=verticalSumMatrix[i][l];
-        }
-        if (tempSum>answer) {
-            answer = tempSum;
-            x = i; y = 1;
-        }
-        for (int j=k+1; j<=n; j++) {
-            tempSum = tempSum + verticalSumMatrix[i][j] - verticalSumMatrix[i][j-k];
-            if (tempSum>answer) {
-                answer = tempSum;
-                x = i; y = j-k+1;
+    int maxVal = INT_MIN;
+    for (int m=1; m<=c; m++) {
+        for (int n=1; n<=c; n++) {
+            vector<int>tempVec;
+            for(int i=1; i<=n; i++) {
+                int sum = 0;
+                for (int j=m; j<=n; j++){
+                    sum+=matrix[i][j];
+                }
+                tempVec.push_back(sum);
             }
+            int returnedVal = kadane(tempVec);
+            maxVal = maxVal<returnedVal?returnedVal:maxVal;
         }
     }
-    for(int i=x; i<=x+k-1; i++) {
-        for(int j=y; j<=y+k-1; j++) {
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    
+    cout<<maxVal<<endl;
+    
     
     return 0;
 }
 
 
+    
