02e1586d77f6cf43a88ab79cecf8333903193421
Mithoon Kumar <mithoonk@zeta.tech>
2019-01-02 21:58:10 +0530

    Implement code

--- code/main.cpp
+++ code/main.cpp
@@ -15,28 +15,70 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
-void printAllPermutations(string str, string pStr, bool visited[], int tempLength, int length) {
-    if(tempLength == length) {
-        cout<<pStr<<endl;
-        return;
+struct point {
+    int ele, dis;
+    point(int ele, int dis) {
+        this->ele = ele;
+        this->dis = dis;
     }
-    for(int i=0; i<str.length(); i++) {
-        if(!visited[i]) {
-            pStr.push_back(str[i]);
-            visited[i] = true;
-            printAllPermutations(str, pStr, visited , tempLength+1, length);
-            pStr.pop_back();
-            visited[i] = false;
+};
+
+int getMinDiceThrows(int moves[], int N) {
+    bool visited[N];
+    for(int i=0; i<N; i++) {
+        visited[i] = false;
+    }
+    queue<point>q;
+    int startingCoordinate = (moves[0] == -1 ? 0 : moves[0]);
+    point starting = point(startingCoordinate, 0);
+    q.push(starting);
+    visited[0] = true;
+    int answer = -1;
+    while(!q.empty()) {
+        point topElement = q.front();
+        q.pop();
+        for (int i= topElement.ele+1; i<=topElement.ele+6; i++) {
+            if (i>=N) {
+                continue;
+            }
+            int nextPoint = (moves[i] == -1 ? i : moves[i]);
+            if (!visited[nextPoint]) {
+                point tempPoint = point(nextPoint, topElement.dis+1);
+                q.push(tempPoint);
+                visited[nextPoint] = true;
+                if (tempPoint.ele == N-1) {
+                    answer = tempPoint.dis;
+                    break;
+                }
+            }
         }
     }
+    return answer;
 }
+
 
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
-    string str = "abc", permutedString="";
-    bool visited[3] = {};
-    printAllPermutations(str, permutedString, visited, 0, 3);
+    int N = 30;
+    int moves[N];
+    for (int i = 0; i<N; i++)
+        moves[i] = -1;
+    
+    // Ladders
+    moves[2] = 21;
+    moves[4] = 7;
+    moves[10] = 25;
+    moves[19] = 28;
+    
+    // Snakes
+    moves[26] = 0;
+    moves[20] = 8;
+    moves[16] = 3;
+    moves[18] = 6;
+    
+    cout << "Min Dice throws required is " << getMinDiceThrows(moves, N);
     return 0;
 }
+
 
