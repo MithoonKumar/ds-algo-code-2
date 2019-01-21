a0a2bc6c2a46b5fb339db7b8ef67b7fd2ade8898
Mithoon Kumar <mithoonk@zeta.tech>
2019-01-21 10:52:15 +0530

    WIP

--- code/main.cpp
+++ code/main.cpp
@@ -15,64 +15,33 @@
 using namespace std;
 #define faster  ios_base::sync_with_stdio(false); cin.tie(NULL)
 
-struct trieNode {
-    bool isEndOfWord;
-    trieNode * chidlren[26];
-    trieNode() {
-        this->isEndOfWord = false;
-        for (int i=0; i<26; i++) {
-            this->chidlren[i] = NULL;
-        }
-    }
-};
-
-
-
-
-void insert(string str, int pos, trieNode * root) {
-    if (root->chidlren[str[pos]-'a'] == NULL) {
-        root->chidlren[str[pos]-'a'] = new trieNode();
-    }
-    if (pos+1 == str.length()) {
-        root->chidlren[str[pos]-'a']->isEndOfWord = true;
+bool compareFunc(string str1, string str2) {
+    string str3 = str1 + str2;
+    string str4 = str2 + str1;
+    if (str3 > str4) {
+        return true;
     } else {
-        insert(str, pos+1, root->chidlren[str[pos]-'a']);
-    }
-}
-
-bool searchT(string str, int pos, trieNode * root) {
-    if (!root->chidlren[str[pos]-'a']) {
         return false;
-    } else {
-        if (pos+1 == str.length()) {
-            if (root->chidlren[str[pos]-'a']->isEndOfWord) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return searchT(str, pos+1, root->chidlren[str[pos]-'a']);
-        }
     }
 }
-
 
 int main(){
     freopen("/Users/mithoon.k/Documents/github-repo/ds-algo-code/code/code/input.txt","r",stdin);
     faster;
     int n;
     cin>>n;
-    string strings[n];
-    trieNode * root = new trieNode();
-    for (int i=0; i<n; i++) {
-        cin>>strings[i];
-        insert(strings[i],0, root);
+    vector<string> vec;
+    for(int i=0; i<n; i++) {
+        string temp;
+        cin>>temp;
+        vec.push_back(temp);
     }
-    
-    searchT("the",0, root)? cout<<"found"<<endl :cout<< "not found"<<endl;
-    searchT("these",0, root)? cout<<"found"<<endl :cout<< "not found"<<endl;
-    searchT("their", 0, root)?cout<< "found"<<endl :cout<< "not found"<<endl;
-    searchT("thaw", 0, root)?cout<< "found"<<endl :cout<< "not found"<<endl;
+    sort(vec.begin(), vec.end(), compareFunc);
+    string answer;
+    for(int i=0; i<n; i++) {
+        answer = answer + vec[i];
+    }
+    cout<<answer<<endl;
     
     return 0;
 }
